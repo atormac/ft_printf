b463fab8e95e38748cb8cd9c9ff08cf66d67f3c2
Aarni Torma <atorma@student.hive.fi>
2024-04-22 19:28:55 +0300

    counter

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: atorma <atorma@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/22 17:47:24 by atorma            #+#    #+#             */
-/*   Updated: 2024/04/22 18:59:37 by atorma           ###   ########.fr       */
+/*   Updated: 2024/04/22 19:28:31 by atorma           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,22 +16,6 @@
 #include <unistd.h>
 #include <limits.h>
 #include "libft.h"
-
-int	count_args(const char *f)
-{
-	int	count;
-
-	count = 0;
-	while (*f)
-	{
-		if (*f == '%')
-		{
-			count++;
-		}
-		f++;
-	}
-	return (count);
-}
 
 void	print_uint(unsigned int n)
 {
@@ -43,51 +27,95 @@ void	print_uint(unsigned int n)
 	ft_putchar_fd('0' + (num % 10), 1);
 }
 
-void	format_print(va_list ap, const char *f)
+int	ft_int_len(long long num)
 {
+	int	length;
+
+	length = 1;
+	if (num < 0)
+	{
+		length++;
+		num *= -1;
+	}
+	while (num >= 10)
+	{
+		num /= 10;
+		length++;
+	}
+	return (length);
+}
+int print_char(int c)
+{
+	ft_putchar_fd(c, 1);
+	return (1);
+}
+
+int	print_string(char *str)
+{
+	int	len;
+
+	len = ft_strlen(str);
+	ft_putstr_fd(str, 1);
+	return (len);
+}
+
+int print_integer(int n)
+{
+	ft_putnbr_fd(n, 1);
+	return (ft_int_len(n));
+}
+
+int	format_print(va_list ap, const char *f)
+{
+	int	counter;
+
+	counter = 0;
 	if (*f == 'c')
-		ft_putchar_fd(va_arg(ap, int), 1);
+		counter += print_char(va_arg(ap, int));
 	else if (*f == '%')
-		ft_putchar_fd('%', 1);
+		counter += print_char('%');
 	else if (*f == 's')
-		ft_putstr_fd(va_arg(ap, char*), 1);
+		counter += print_string(va_arg(ap, char*));
 	else if (*f == 'd' || *f == 'i')
-		ft_putnbr_fd(va_arg(ap, int), 1);
+		counter += print_integer(va_arg(ap, int));
 	else if (*f == 'u')
 		print_uint(va_arg(ap, unsigned int));
+	return (counter);
 }
+
 int ft_printf(const char *f, ...)
 {
 	va_list ap;
-	int	count_printed;
+	int		counter;
 
 	va_start(ap, f);
-	count_printed = 0;
+	counter = 0;
 	while (*f)
 	{
 		if (*f == '%')
 		{
 			f++;
-			format_print(ap, f);
+			counter += format_print(ap, f);
 		}
 		else
 		{
 			ft_putchar_fd(*f, 1);
-			count_printed++;
+			counter++;
 		}
 		f++;
 	}
 	va_end(ap);
-	return (count_printed);
+	return (counter);
 }
 
 int main(void)
 {
 	void *p = format_print;
 
-	ft_printf("format: %%, %s, %c, %s, %d, %i, %u\n", "str", 'b', "second", 12345, 123, UINT_MAX);
-	ft_printf("no args\n");
+	int counter = ft_printf("format: %%, %s, %c, %s, %d, %i, %u\n", "str", 'b', "second", 12345, 123, UINT_MAX);
+	ft_printf("counter: %d\n", counter);
 	write(1, "\n", 1);
-	printf("format: %%, %s, %c, %s, %d, %i, %u\n", "str", 'b', "second", 12345, 123, UINT_MAX);
+	int printf_counter = printf("format: %%, %s, %c, %s, %d, %i, %u\n", "str", 'b', "second", 12345, 123, UINT_MAX);
+	printf("printf_counter: %d\n", printf_counter);
 	return (0);
 }
